783bd5dfd8a87e32181f697699fcb106b1b65444
Pierre-Gilles Levallois <pgl@erasme.org>
2015-09-30 14:49:15 +0200

    meilleure présentation des carrés de regroupement

--- docCtrl.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-/******************************************* Doc Controller ****************************************/
-Controllers.controller('DocCtrl', ['$scope', '$state', function($scope, $state){
-    
-    $scope.pdfUrl = 'api/publipostage/'+$state.params['id']+'/pdf';
-}]);
